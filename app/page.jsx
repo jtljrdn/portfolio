a9f239c700e21651b4d5a2a2ccf58a08352ca033
Jordan Lee <jtlee@rmgj2.com>
2023-10-23 22:04:35 -0500

    overhauled style, changed to react and next.js

---
+++ app/page.jsx
@@ -0,0 +1,23 @@
+import CardFeed from "@/components/CardFeed";
+
+const page = () => {
+  return (
+    <div>
+        <section className="w-full flex-center flex-col mt-10">
+      <h1 className="head_text blue_gradient text-center">Jordan Lee</h1>
+      <p className="text-center desc mb-5">
+        Welcome to my personal portfolio! Below is some of my
+        experience/personal projects
+      </p>
+      </section>
+  <div class="divider"></div> 
+
+      <h2 className="sub_text text-center mt-3"><span className="green_gradient">Experience</span> and <span className="orange_gradient">Projects</span></h2>
+      <div >
+        <CardFeed />
+      </div>
+    </div>
+  );
+};
+
+export default page;
