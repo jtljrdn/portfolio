7630f356c58e51eb9d076b9f1f28b9ecefe09bf6
Jordan Lee <jtlee@rmgj2.com>
2024-05-04 02:33:57 -0500

    Update FontAwesome package versions and refactor Card component in Card.jsx

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,41 +1,39 @@
 import Image from "next/image";
 import Link from "next/link";
 
-const Card = ({ name, dates, data, link, picture, imgSource }) => {
-  if (!picture) {
-    return (
-      <Link href={link}>
-        <div className="card w-72 bg-base-100 shadow-xl hover">
-          <div className="card-body">
-            <h2 className="card-title">{name}</h2>
-            <p>{data}</p>
-            <div className="card-actions justify-end"></div>
-          </div>
+const Card = ({ href, children }) => {
+  return (
+    <Link href={href} target="blank">
+      <div className=" card w-72 bg-base-100 shadow-xl hover">
+        <div className="card-body">
+          {children}
+          <div className="card-actions justify-end"></div>
         </div>
-      </Link>
-    );
-  } else {
-    return (
-      <Link href={link} target="blank">
-        <div className="card w-72 bg-base-100 shadow-xl hover">
-          <figure className="px-10 pt-10">
-            <Image
-              src={imgSource}
-              width={300}
-              height={200}
-              className="rounded-xl"
-            />
-          </figure>
-          <div className="card-body">
-            <h2 className="card-title">{name}</h2>
-            <div className="badge badge-neutral">{dates}</div>
-            <p>{data}</p>
-            <div className="card-actions justify-end"></div>
-          </div>
-        </div>
-      </Link>
-    );
-  }
+      </div>
+    </Link>
+  );
 };
 
-export default Card;
+const CardHeader = ({ name, dates, title }) => {
+  return (
+    <>
+      <h2 className="card-title">{name}</h2>
+      <h3 className="text-m -mt-2.5 text-gray-600">{title}</h3>
+      {dates && <div className="badge badge-neutral">{dates}</div>}
+    </>
+  );
+};
+
+const CardImage = ({ imgSource }) => {
+  return (
+    <>
+      <Image src={imgSource} width={300} height={200} className="rounded-xl" />
+    </>
+  );
+};
+
+const CardBody = ({ children }) => {
+  return (<div>{children}</div>);
+};
+
+export { Card, CardHeader, CardImage, CardBody };
